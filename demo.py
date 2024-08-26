74850a03e82f212bf82fe40b7c2e77af64d4bdb3
Nuwan Nadeera <47295198+nuwannadeera@users.noreply.github.com>
2024-08-26 17:10:13 +0530

    Initial commit

---
+++ demo.py
@@ -0,0 +1,21 @@
+# print("Hello Python..")
+# print(123)
+# print("saman said \n\"fdgdfgdgdfgd\" ")
+
+name = 'djjjjjjjjjj'
+# print(name)
+
+
+a = "aa"
+b = "bb"
+c = 5
+
+#d2 = a + b + c
+#print(d2)  # it shows error, it does not allow direct concatenation of a string and an integer.
+
+d1 = a + b + str(c)
+print(d1)
+
+x = 10
+y = 2.3333
+print(type(x),type(y)) # can print more by adding ,
